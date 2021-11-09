7a907664cdc87b5697509465ad04d11cfe9d475c
Ritik Kanotra <ritikkanotra@gmail.com>
2021-11-09 21:59:53 +0530

    Add app version text in settings

--- app/src/main/java/com/appodex/eventauth/SettingsFragment.java
+++ app/src/main/java/com/appodex/eventauth/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.appodex.eventauth;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,6 +19,8 @@ public class SettingsFragment extends Fragment {
 
     private LinearLayout logoutBtn;
     private FirebaseAuth mAuth;
+    private TextView appVersionTV;
+    private String versionName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +32,15 @@ public class SettingsFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
 
         logoutBtn = view.findViewById(R.id.ll_logout);
+        appVersionTV = view.findViewById(R.id.tv_app_version);
+
+        try {
+            PackageInfo packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            versionName = packageInfo.versionName;
+            appVersionTV.setText(versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
         logoutBtn.setOnClickListener(task -> {
